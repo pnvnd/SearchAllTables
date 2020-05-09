ca978c60ab519f0383184531514a0cbc289c4142
pnvnd <pvnn@msn.com>
2020-05-09 17:44:48 -0400

    Changed WHILE loop to CURSOR for better performance

--- SearchAllTables.sql
+++ SearchAllTables.sql
@@ -2,74 +2,75 @@
 -- CREATE PROC SearchAllTables
 
 -- Replace 'string' with whatever you text you need to search for
-	DECLARE @SearchStr nvarchar(100) = 'string'
+DECLARE @searchStr nvarchar(100) = 'conduit'
 
 -- AS
 -- BEGIN
 
-    -- Modified by: Peter Nguyen https://github.com/pnvnd/SearchAllTables
-	-- Tested with SQL Server: 2008, 2008 R2, 2012, 2014, 2017
+-- Modified by: Peter Nguyen https://github.com/pnvnd/SearchAllTables
+-- Tested with SQL Server: 2008, 2008 R2, 2012, 2014, 2017
 
-	-- Copyright © 2002 Narayana Vyas Kondreddi. All rights reserved.
-	-- Purpose: To search all columns of all tables for a given search string
-	-- Written by: Narayana Vyas Kondreddi
-	-- Site: http://vyaskn.tripod.com
-	-- Tested on: SQL Server 7.0 and SQL Server 2000
-	-- Date modified: 28th July 2002 22:50 GMT
+-- Copyright © 2002 Narayana Vyas Kondreddi. All rights reserved.
+-- Purpose: To search all columns of all tables for a given search string
+-- Written by: Narayana Vyas Kondreddi
+-- Site: http://vyaskn.tripod.com
+-- Tested on: SQL Server 7.0 and SQL Server 2000
+-- Date modified: 28th July 2002 22:50 GMT
 
-	CREATE TABLE #Results (ColumnName nvarchar(370), ColumnValue nvarchar(3630))
+-- Create a temporary table to select records out of when query complete
+CREATE TABLE #Results
+	(
+		tableName nvarchar(256),
+		columnName nvarchar(370),
+		columnValue nvarchar(3630),
+		searchStr nvarchar(110)
+	)
 
-	SET NOCOUNT ON
+SET NOCOUNT ON;
 
-	DECLARE @TableName nvarchar(256), @ColumnName nvarchar(128), @SearchStr2 nvarchar(110)
-	SET  @TableName = ''
-	SET @SearchStr2 = QUOTENAME('%' + @SearchStr + '%','''')
+DECLARE
+	@tableName nvarchar(256),
+	@columnName nvarchar(128)
 
-	WHILE @TableName IS NOT NULL
-	BEGIN
-		SET @ColumnName = ''
-		SET @TableName = 
-		(
-			SELECT MIN(QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME))
-			FROM 	INFORMATION_SCHEMA.TABLES
-			WHERE 		TABLE_TYPE = 'BASE TABLE'
-				AND	QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME) > @TableName
-				AND	OBJECTPROPERTY(
-						OBJECT_ID(
-							QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME)
-							 ), 'IsMSShipped'
-						       ) = 0
-		)
+BEGIN
+	DECLARE search CURSOR
+	FOR
+	-- Select columns in base tables where the data type is some kind of text
+		SELECT tbl.TABLE_NAME, col.COLUMN_NAME
+		FROM INFORMATION_SCHEMA.COLUMNS col
+			INNER JOIN INFORMATION_SCHEMA.TABLES tbl
+				ON col.TABLE_NAME = tbl.TABLE_NAME
+		WHERE DATA_TYPE IN ('char', 'varchar', 'nchar', 'nvarchar')
+			AND col.COLUMN_NAME <> 'DEX_ROW_ID'
+			AND tbl.TABLE_TYPE = 'BASE TABLE'
+		ORDER BY tbl.TABLE_NAME, col.COLUMN_NAME
 
-		WHILE (@TableName IS NOT NULL) AND (@ColumnName IS NOT NULL)
-		BEGIN
-			SET @ColumnName =
-			(
-				SELECT MIN(QUOTENAME(COLUMN_NAME))
-				FROM 	INFORMATION_SCHEMA.COLUMNS
-				WHERE 		TABLE_SCHEMA	= PARSENAME(@TableName, 2)
-					AND	TABLE_NAME	= PARSENAME(@TableName, 1)
-					AND	DATA_TYPE IN ('char', 'varchar', 'nchar', 'nvarchar')
-					AND	QUOTENAME(COLUMN_NAME) > @ColumnName
-			)
-	
-			IF @ColumnName IS NOT NULL
+	OPEN search
+		FETCH NEXT FROM Search INTO @tableName, @columnName
+
+		WHILE @@FETCH_STATUS = 0
+
 			BEGIN
-				INSERT INTO #Results
-				EXEC
-				(
-					'SELECT ''' + @TableName + '.' + @ColumnName + ''', LEFT(' + @ColumnName + ', 3630) 
-					FROM ' + @TableName + ' (NOLOCK) ' +
-					' WHERE ' + @ColumnName + ' LIKE ' + @SearchStr2
-				)
-			END
-		END	
-	END
 
-	SELECT ColumnName, ColumnValue FROM #Results
+				DECLARE @sqlStmtStr1 varchar(1024)
+				SET @sqlStmtStr1 = 'SELECT ''' + @tableName + ''', ''' + @columnName + ''', LEFT(' + @columnName + ', 3072), ''''  
+									FROM ' + @tableName + ' (NOLOCK) ' + ' WHERE ' + @columnName + ' LIKE ' + '''%' +  @searchStr  + '%'''
+		
+				IF @columnName IS NOT NULL
+						BEGIN
+							INSERT INTO #results EXEC (@sqlStmtStr1)
+						END
+				
+		FETCH NEXT FROM Search INTO @tableName, @columnName
+		END
+	CLOSE search
+	DEALLOCATE search
+END
 
-	-- Drop table so you can run search on demand again
-	DROP TABLE #Results
+SELECT DISTINCT tableName, columnName, columnValue FROM #results
+
+-- Drop table so you can run search on demand again
+DROP TABLE #results
 
 -- Comment out last END since we don't want a stored procedure
 -- END
